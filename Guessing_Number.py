e93649c7cc8a427685d20c16de2848ad229792d7
sudorituraj <150174725+sudorituraj@users.noreply.github.com>
2024-11-05 19:25:05 +0530

    Add files via upload

---
+++ Guessing_Number.py
@@ -0,0 +1,46 @@
+import random
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please type a number larger then 0 next time.")
+        quit()
+
+else:
+    print("Please enter a number Next time ")
+    quit()
+
+random_number = random.randint(0, top_of_range)
+guesses = 0
+
+while True :
+    guesses += 1
+    user_guess = input("Make a guess (or type 'q' to quit): ")
+
+    if user_guess.lower() == 'q' or user_guess.lower() == 'exit':
+        print("You chose to quit the game. Goodbye!")
+        break
+    
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+
+
+    else:
+        print("Please type a number next time. ")
+        continue 
+        quit()
+    
+    if user_guess == random_number:
+        print("You got it ")
+        break
+    elif user_guess > random_number:
+        print("You are above the number!")
+
+    else :
+        print("You were below the number! ")
+
+
+print("You got it in", guesses, "guesses")
